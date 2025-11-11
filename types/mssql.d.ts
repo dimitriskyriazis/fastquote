806c75bf9a55c3164d192498d17983a02de14766
Dimitris <dk2g21@soton.ac.uk>
2025-11-11 14:40:05 +0200

    fixed build error

--- types/mssql.d.ts
+++ types/mssql.d.ts
@@ -17,6 +17,7 @@ declare module 'mssql' {
 
   export interface IResult<T> {
     recordset: T[];
+    rowsAffected?: number[];
   }
 
   export class ConnectionPool {
@@ -32,7 +33,10 @@ declare module 'mssql' {
     query<T = unknown>(query: string): Promise<IResult<T>>;
   }
 
+  type SqlTypeFactory = (...args: unknown[]) => unknown;
+
   export const Int: symbol;
+  export const NVarChar: SqlTypeFactory;
 
   export function connect(config: config): Promise<ConnectionPool>;
 
@@ -40,6 +44,7 @@ declare module 'mssql' {
     connect: typeof connect;
     ConnectionPool: typeof ConnectionPool;
     Int: typeof Int;
+    NVarChar: typeof NVarChar;
   };
 
   export default sql;
