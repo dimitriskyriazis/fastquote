4d619b9eac608866aadf144d6fbedf75c3941246
Dimitris <dk2g21@soton.ac.uk>
2026-03-09 13:26:47 +0200

    test fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  serverExternalPackages: ['mssql', 'msnodesqlv8'],
   async headers() {
     return [
       {
